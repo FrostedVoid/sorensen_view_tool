4490c9458da1945da45a3d90f853ae642554b2d6
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 16:22:43 +0000

    Sorensen Tool Initial Commit

---
+++ lib/sorensen_view_tool.rb
@@ -0,0 +1,6 @@
+require "sorensen_view_tool/version"
+require "sorensen_view_tool/renderer"
+
+module SorensenViewTool
+  # Your code goes here...
+end
---
+++ lib/sorensen_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SorensenViewTool
+  VERSION = "0.1.0"
+end
